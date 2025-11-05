26955b468f621969bef3672e9556bb08c3f7df22
AdvikGarg <gargadvik23@gmail.com>
2025-11-05 22:11:00 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,8 @@
+print("Hello world")
+
+#printing a number
+print(3)
+
+print("My name is Advik \nI study in class 7")
+print("coding",9)
+print("Welcome to ",end = "*") 
---
+++ variables.py
@@ -0,0 +1,11 @@
+#creating variables
+x=4
+y="Advik"
+print(x)
+print(y)
+
+codingal = "python"
+print(codingal)
+
+name = input("My name is:")
+print("My name is: ", name)
